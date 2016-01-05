1847638654f6f7121b4fa60bb8b46c9d413524ff
Kevin Kaiser <s151457@student.dtu.dk>
2016-01-05 17:02:40 +0100

    commands added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # HPC-Assignment
 Assignement in High Performance Computing class at DTU
+
+# push with:
+/appl/git/2.6.1/bin/git push -u origin master
--- assignment1/libmatmult.c
+++ assignment1/libmatmult.c
@@ -2,30 +2,77 @@
 #include <stdlib.h>
 #include <cblas.h>
 
-void matmult_lib(int m, int n, int k, double **A, double **B, double **C) {
+void
+matmult_lib (int m, int n, int k, double **A, double **B, double **C)
+{
 
-    double alpha = 1.0;
-    double beta = 0.0;
-    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, alpha, *A, k, *B, n, beta, *C, n);
+  double alpha = 1.0;
+  double beta = 0.0;
+  cblas_dgemm (CblasRowMajor, CblasNoTrans, CblasNoTrans, m, n, k, alpha, *A,
+	       k, *B, n, beta, *C, n);
 }
 
-double **matmult_nat(int m, int n, int k, double **A, double **B, double **C) {
+// natural C ordering
+double **
+matmult_nat (int m, int n, int k, double **A, double **B, double **C)
+{
 
-	int i, j , r, sum;
+  int i, j, r, sum;
 
-   	for (i = 0; i < m; i++) {
-   		for (j = 0; j < n; j++) {
-        	sum = 0;
-         	for (r = 0; r < k; r++) {
-            	sum = sum + A[i][r] * B[r][j];
-         	}
-         	C[i][j] = sum;
-      	}
-   	}
+  for (i = 0; i < m; i++)
+    {
+      for (j = 0; j < n; j++)
+	{
+	  sum = 0;
+	  for (r = 0; r < k; r++)
+	    {
+	      sum = sum + A[i][r] * B[r][j];
+	    }
+	  C[i][j] = sum;
+	}
+    }
 
-	return C;
+  return C;
 }
 
-double **matmult_blk(int m,int n,int k,double *A,double *B,double *C, int bs) {
+double **
+matmult_blk (int m, int n, int k, double *A, double *B, double *C, int bs)
+{
+
+}
+
+double **
+matmult_mnk (int m, int n, int k, double *A, double *B, double *C)
+{
+
+}
+
+double **
+matmult_mkn (int m, int n, int k, double *A, double *B, double *C)
+{
+
+}
+
+double **
+matmult_knm (int m, int n, int k, double *A, double *B, double *C)
+{
+
+}
+
+double **
+matmult_kmn (int m, int n, int k, double *A, double *B, double *C)
+{
+
+}
+
+double **
+matmult_nmk (int m, int n, int k, double *A, double *B, double *C)
+{
+
+}
+
+double **
+matmult_nkm (int m, int n, int k, double *A, double *B, double *C)
+{
 
 }
